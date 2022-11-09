f05f7f9cd84d90c62ce2f8113cd0b0acb2577025
Edanur Uludag <100486387+Eda96@users.noreply.github.com>
2022-11-09 01:07:32 +0100

    Update catsPage.js

--- edaCatApp/src/cats/catsPage.js
+++ edaCatApp/src/cats/catsPage.js
@@ -1,17 +1,17 @@
 import { catsView } from "./catsView.js";
 
-export const initCatsPage = (number) => {
-  const fetchData = async (number) => {
+export const initCatsPage = (parameter) => {
+  const fetchData = async (parameter) => {
     try {
-      if ((number === "10") | (number === "20") | (number === "30")) {
+      if ((parameter === "10") | (parameter === "20") | (parameter === "30")) {
         const response = await fetch(
-          `https://api.thecatapi.com/v1/images/search?limit=${number}&has_breeds=1&api_key=live_CEKMh36HMAEeGUD8Z97uq9SwbKtiLgf0jarW4kSwrKO8x35lxOs0C0KZMUXZpiy0`
+          `https://api.thecatapi.com/v1/images/search?limit=${parameter}&has_breeds=1&api_key=live_CEKMh36HMAEeGUD8Z97uq9SwbKtiLgf0jarW4kSwrKO8x35lxOs0C0KZMUXZpiy0`
         );
         const data = await response.json();
         view.showCats(data);
       } else {
         const response = await fetch(
-          `https://api.thecatapi.com/v1/images/search?breed_ids=${number}&api_key=live_CEKMh36HMAEeGUD8Z97uq9SwbKtiLgf0jarW4kSwrKO8x35lxOs0C0KZMUXZpiy0`
+          `https://api.thecatapi.com/v1/images/search?breed_ids=${parameter}&api_key=live_CEKMh36HMAEeGUD8Z97uq9SwbKtiLgf0jarW4kSwrKO8x35lxOs0C0KZMUXZpiy0`
         );
         const data = await response.json();
         view.showCats(data);
@@ -21,7 +21,8 @@ export const initCatsPage = (number) => {
     }
   };
 
-  fetchData(number);
+  fetchData(parameter);
   const view = catsView();
   return view;
 };
+
